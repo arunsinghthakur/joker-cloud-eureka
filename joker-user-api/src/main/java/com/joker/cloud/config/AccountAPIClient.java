f8fc7168109c59f77f778da8eaf82a38cadca003
arunsingh <arunsingh@Arun373501.Nagarro.local>
2018-07-09 12:23:16 +0530

    aa

--- joker-user-api/src/main/java/com/joker/cloud/config/AccountAPIClient.java
+++ joker-user-api/src/main/java/com/joker/cloud/config/AccountAPIClient.java
@@ -2,10 +2,11 @@ package com.joker.cloud.config;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient("JOKER-ACCOUNT-API")
+@FeignClient("joker-account-api")
 public interface AccountAPIClient{
 	
-	@RequestMapping("/rest/account/info")
+	@RequestMapping(method=RequestMethod.GET, value = "/rest/account/info")
 	public String acccountNumber();
 }
